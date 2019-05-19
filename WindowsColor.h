d930643683ecd0273976a8a5e1e376e6a5d19ab1
bohanjun <41739624+bohanjun@users.noreply.github.com>
2019-05-19 20:44:20 +0800

    <2.0>WindowsColor.h

--- WindowsColor.h
+++ WindowsColor.h
@@ -1,149 +1,179 @@
 //包含系统头文件 
 #include<windows.h>
+#include<ostream>
+#include<istream>
+#include<cstdio>
+
+//令人着迷的【Class】【欧，你太可爱了！】 
+class Color{
+	public:
+		WORD VALUE;
+		Color(const WORD&num=0x00){*this=num;}
+		Color(const Color&_COLOR){this->VALUE=_COLOR.VALUE;}
+		Color&operator=(WORD num){
+			assert(num>=0x00&&num<=0xFF);
+			VALUE=num;
+			return*this;
+		}
+		Color&operator=(Color _COLOR){
+			VALUE=_COLOR.VALUE;
+			return*this;
+		}
+		Color operator+(const Color&_COLOR)const{
+			Color tmp;
+			tmp.VALUE=VALUE+_COLOR.VALUE;
+			return tmp;
+		}
+		Color operator|(const Color&_COLOR)const{
+			Color tmp;
+			tmp.VALUE=VALUE|_COLOR.VALUE;
+			return tmp;
+		}
+};
 
 //定义各种各样奇奇怪怪的万分神奇的【颜色常量】 
 //定义颜色常量 
 //前景 
-const WORD BLACK_FG     =0x0;
-const WORD BLUE_FG      =0x1;
-const WORD GRASS_FG     =0x2;
-const WORD AZURE_FG     =0x3;
-const WORD BLOOD_FG     =0x4;
-const WORD VIOLET_FG    =0x5;
-const WORD BROWN_FG     =0x6;
-const WORD WHITE_FG     =0x7;
-const WORD GREY_FG      =0x8;
-const WORD INDIGO_FG    =0x9;
-const WORD GREEN_FG     =0xA;
-const WORD CYAN_FG      =0xB;
-const WORD RED_FG       =0xC;
-const WORD MAGENTA_FG   =0xD;
-const WORD YELLOW_FG    =0xE;
-const WORD SNOW_FG      =0xF;
+const Color BLACK_FG     =0x0;
+const Color BLUE_FG      =0x1;
+const Color GRASS_FG     =0x2;
+const Color AZURE_FG     =0x3;
+const Color BLOOD_FG     =0x4;
+const Color VIOLET_FG    =0x5;
+const Color BROWN_FG     =0x6;
+const Color WHITE_FG     =0x7;
+const Color GREY_FG      =0x8;
+const Color INDIGO_FG    =0x9;
+const Color GREEN_FG     =0xA;
+const Color CYAN_FG      =0xB;
+const Color RED_FG       =0xC;
+const Color MAGENTA_FG   =0xD;
+const Color YELLOW_FG    =0xE;
+const Color SNOW_FG      =0xF;
 //前景_兼容缩写 
-const WORD BLACK        =0x0;
-const WORD BLUE         =0x1;
-const WORD GRASS        =0x2;
-const WORD AZURE        =0x3;
-const WORD BLOOD        =0x4;
-const WORD VIOLET       =0x5;
-const WORD BROWN        =0x6;
-const WORD WHITE        =0x7;
-const WORD GREY         =0x8;
-const WORD INDIGO       =0x9;
-const WORD GREEN        =0xA;
-const WORD CYAN         =0xB;
-const WORD RED          =0xC;
-const WORD MAGENTA      =0xD;
-const WORD YELLOW       =0xE;
-const WORD SNOW         =0xF;
+const Color BLACK        =0x0;
+const Color BLUE         =0x1;
+const Color GRASS        =0x2;
+const Color AZURE        =0x3;
+const Color BLOOD        =0x4;
+const Color VIOLET       =0x5;
+const Color BROWN        =0x6;
+const Color WHITE        =0x7;
+const Color GREY         =0x8;
+const Color INDIGO       =0x9;
+const Color GREEN        =0xA;
+const Color CYAN         =0xB;
+const Color RED          =0xC;
+const Color MAGENTA      =0xD;
+const Color YELLOW       =0xE;
+const Color SNOW         =0xF;
 //背景 
-const WORD BLACK_BG     =0x00;
-const WORD BLUE_BG      =0x10;
-const WORD GRASS_BG     =0x20;
-const WORD AZURE_BG     =0x30;
-const WORD BLOOD_BG     =0x40;
-const WORD VIOLET_BG    =0x50;
-const WORD BROWN_BG     =0x60;
-const WORD WHITE_BG     =0x70;
-const WORD GREY_BG      =0x80;
-const WORD INDIGO_BG    =0x90;
-const WORD GREEN_BG     =0xA0;
-const WORD CYAN_BG      =0xB0;
-const WORD RED_BG       =0xC0;
-const WORD MAGENTA_BG   =0xD0;
-const WORD YELLOW_BG    =0xE0;
-const WORD SNOW_BG      =0xF0;
+const Color BLACK_BG     =0x00;
+const Color BLUE_BG      =0x10;
+const Color GRASS_BG     =0x20;
+const Color AZURE_BG     =0x30;
+const Color BLOOD_BG     =0x40;
+const Color VIOLET_BG    =0x50;
+const Color BROWN_BG     =0x60;
+const Color WHITE_BG     =0x70;
+const Color GREY_BG      =0x80;
+const Color INDIGO_BG    =0x90;
+const Color GREEN_BG     =0xA0;
+const Color CYAN_BG      =0xB0;
+const Color RED_BG       =0xC0;
+const Color MAGENTA_BG   =0xD0;
+const Color YELLOW_BG    =0xE0;
+const Color SNOW_BG      =0xF0;
 //定义颜色常量_保留 
 //前景_保留 
-const WORD _BLACK_FG    =0x0;
-const WORD _BLUE_FG     =0x1;
-const WORD _GRASS_FG    =0x2;
-const WORD _AZURE_FG    =0x3;
-const WORD _BLOOD_FG    =0x4;
-const WORD _VIOLET_FG   =0x5;
-const WORD _BROWN_FG    =0x6;
-const WORD _WHITE_FG    =0x7;
-const WORD _GREY_FG     =0x8;
-const WORD _INDIGO_FG   =0x9;
-const WORD _GREEN_FG    =0xA;
-const WORD _CYAN_FG     =0xB;
-const WORD _RED_FG      =0xC;
-const WORD _MAGENTA_FG  =0xD;
-const WORD _YELLOW_FG   =0xE;
-const WORD _SNOW_FG     =0xF;
+const Color _BLACK_FG    =0x0;
+const Color _BLUE_FG     =0x1;
+const Color _GRASS_FG    =0x2;
+const Color _AZURE_FG    =0x3;
+const Color _BLOOD_FG    =0x4;
+const Color _VIOLET_FG   =0x5;
+const Color _BROWN_FG    =0x6;
+const Color _WHITE_FG    =0x7;
+const Color _GREY_FG     =0x8;
+const Color _INDIGO_FG   =0x9;
+const Color _GREEN_FG    =0xA;
+const Color _CYAN_FG     =0xB;
+const Color _RED_FG      =0xC;
+const Color _MAGENTA_FG  =0xD;
+const Color _YELLOW_FG   =0xE;
+const Color _SNOW_FG     =0xF;
 //前景_兼容缩写_保留 
-const WORD _BLACK       =0x0;
-const WORD _BLUE        =0x1;
-const WORD _GRASS       =0x2;
-const WORD _AZURE       =0x3;
-const WORD _BLOOD       =0x4;
-const WORD _VIOLET      =0x5;
-const WORD _BROWN       =0x6;
-const WORD _WHITE       =0x7;
-const WORD _GREY        =0x8;
-const WORD _INDIGO      =0x9;
-const WORD _GREEN       =0xA;
-const WORD _CYAN        =0xB;
-const WORD _RED         =0xC;
-const WORD _MAGENTA     =0xD;
-const WORD _YELLOW      =0xE;
-const WORD _SNOW        =0xF;
+const Color _BLACK       =0x0;
+const Color _BLUE        =0x1;
+const Color _GRASS       =0x2;
+const Color _AZURE       =0x3;
+const Color _BLOOD       =0x4;
+const Color _VIOLET      =0x5;
+const Color _BROWN       =0x6;
+const Color _WHITE       =0x7;
+const Color _GREY        =0x8;
+const Color _INDIGO      =0x9;
+const Color _GREEN       =0xA;
+const Color _CYAN        =0xB;
+const Color _RED         =0xC;
+const Color _MAGENTA     =0xD;
+const Color _YELLOW      =0xE;
+const Color _SNOW        =0xF;
 //背景_保留 
-const WORD _BLACK_BG    =0x00;
-const WORD _BLUE_BG     =0x10;
-const WORD _GRASS_BG    =0x20;
-const WORD _AZURE_BG    =0x30;
-const WORD _BLOOD_BG    =0x40;
-const WORD _VIOLET_BG   =0x50;
-const WORD _BROWN_BG    =0x60;
-const WORD _WHITE_BG    =0x70;
-const WORD _GREY_BG     =0x80;
-const WORD _INDIGO_BG   =0x90;
-const WORD _GREEN_BG    =0xA0;
-const WORD _CYAN_BG     =0xB0;
-const WORD _RED_BG      =0xC0;
-const WORD _MAGENTA_BG  =0xD0;
-const WORD _YELLOW_BG   =0xE0;
-const WORD _SNOW_BG     =0xF0;
+const Color _BLACK_BG    =0x00;
+const Color _BLUE_BG     =0x10;
+const Color _GRASS_BG    =0x20;
+const Color _AZURE_BG    =0x30;
+const Color _BLOOD_BG    =0x40;
+const Color _VIOLET_BG   =0x50;
+const Color _BROWN_BG    =0x60;
+const Color _WHITE_BG    =0x70;
+const Color _GREY_BG     =0x80;
+const Color _INDIGO_BG   =0x90;
+const Color _GREEN_BG    =0xA0;
+const Color _CYAN_BG     =0xB0;
+const Color _RED_BG      =0xC0;
+const Color _MAGENTA_BG  =0xD0;
+const Color _YELLOW_BG   =0xE0;
+const Color _SNOW_BG     =0xF0;
 //定义颜色常量_奇怪的拼写【喵喵喵？】 
 //前景_奇怪的拼写【喵喵喵？】 
-const WORD DARK_FG      =0x0;
-const WORD SKY_FG       =0x3;
-const WORD PURPLE_FG    =0x5;
-const WORD GRAY_FG      =0x8;
-const WORD LIGHT_FG     =0xF;
+const Color DARK_FG      =0x0;
+const Color SKY_FG       =0x3;
+const Color PURPLE_FG    =0x5;
+const Color GRAY_FG      =0x8;
+const Color LIGHT_FG     =0xF;
 //前景_兼容缩写_奇怪的拼写【喵喵喵？】 
-const WORD DARK         =0x0;
-const WORD SKY          =0x3;
-const WORD PURPLE       =0x5;
-const WORD GRAY         =0x8;
-const WORD LIGHT        =0xF;
+const Color DARK         =0x0;
+const Color SKY          =0x3;
+const Color PURPLE       =0x5;
+const Color GRAY         =0x8;
+const Color LIGHT        =0xF;
 //背景_奇怪的拼写【喵喵喵？】 
-const WORD DARK_BG      =0x00;
-const WORD SKY_BG       =0x30;
-const WORD PURPLE_BG    =0x50;
-const WORD GRAY_BG      =0x80;
-const WORD LIGHT_BG     =0xF0;
+const Color DARK_BG      =0x00;
+const Color SKY_BG       =0x30;
+const Color PURPLE_BG    =0x50;
+const Color GRAY_BG      =0x80;
+const Color LIGHT_BG     =0xF0;
 //定义颜色常量_奇怪的拼写【喵喵喵？】_保留 
 //前景_奇怪的拼写【喵喵喵？】_保留 
-const WORD _DARK_FG     =0x0;
-const WORD _SKY_FG      =0x3;
-const WORD _PURPLE_FG   =0x5;
-const WORD _GRAY_FG     =0x8;
-const WORD _LIGHT_FG    =0xF;
+const Color _DARK_FG     =0x0;
+const Color _SKY_FG      =0x3;
+const Color _PURPLE_FG   =0x5;
+const Color _GRAY_FG     =0x8;
+const Color _LIGHT_FG    =0xF;
 //前景_兼容缩写_奇怪的拼写【喵喵喵？】_保留 
-const WORD _DARK        =0x0;
-const WORD _SKY         =0x3;
-const WORD _PURPLE      =0x5;
-const WORD _GRAY        =0x8;
-const WORD _LIGHT       =0xF;
+const Color _DARK        =0x0;
+const Color _SKY         =0x3;
+const Color _PURPLE      =0x5;
+const Color _GRAY        =0x8;
+const Color _LIGHT       =0xF;
 //背景_奇怪的拼写【喵喵喵？】_保留 
-const WORD _DARK_BG     =0x00;
-const WORD _SKY_BG      =0x30;
-const WORD _PURPLE_BG   =0x50;
-const WORD _GRAY_BG     =0x80;
-const WORD _LIGHT_BG    =0xF0;
+const Color _DARK_BG     =0x00;
+const Color _SKY_BG      =0x30;
+const Color _PURPLE_BG   =0x50;
+const Color _GRAY_BG     =0x80;
+const Color _LIGHT_BG    =0xF0;
 
 //定义【可爱】的令人尊敬的模式常量 
 //定义模式常量 
@@ -166,10 +196,20 @@ using namespace std;
 
 //各种各样【不废话了】的颜色更改函数 
 //颜色更改函数 
-void SetColor(WORD COLOR){SetConsoleTextAttribute(GetStdHandle(-11),COLOR);}
-void SetColor(bool MODE,WORD COLOR,bool _nothing){SetConsoleTextAttribute(GetStdHandle(-11),COLOR<<(MODE<<2));}
-void SetColor(WORD COLOR_FG,WORD COLOR_BG){SetConsoleTextAttribute(GetStdHandle(-11),(COLOR_BG<<4)|COLOR_FG);}
+void SetColor(Color COLOR){SetConsoleTextAttribute(GetStdHandle(-11),COLOR.VALUE);}
+void SetColor(bool MODE,Color COLOR,bool _nothing){SetConsoleTextAttribute(GetStdHandle(-11),COLOR.VALUE<<(MODE<<2));}
+void SetColor(Color COLOR_FG,Color COLOR_BG){SetConsoleTextAttribute(GetStdHandle(-11),(COLOR_BG.VALUE<<4)|COLOR_FG.VALUE);}
 //颜色更改函数_保留 
-void _SetColor(WORD COLOR){SetConsoleTextAttribute(GetStdHandle(-11),COLOR);}
-void _SetColor(bool MODE,WORD COLOR,bool _nothing){SetConsoleTextAttribute(GetStdHandle(-11),COLOR<<(MODE<<2));}
-void _SetColor(WORD COLOR_FG,WORD COLOR_BG){SetConsoleTextAttribute(GetStdHandle(-11),(COLOR_BG<<4)|COLOR_FG);}
+void _SetColor(Color COLOR){SetConsoleTextAttribute(GetStdHandle(-11),COLOR.VALUE);}
+void _SetColor(bool MODE,Color COLOR,bool _nothing){SetConsoleTextAttribute(GetStdHandle(-11),COLOR.VALUE<<(MODE<<2));}
+void _SetColor(Color COLOR_FG,Color COLOR_BG){SetConsoleTextAttribute(GetStdHandle(-11),(COLOR_BG.VALUE<<4)|(COLOR_FG.VALUE));}
+
+//【迷人的】的便携式颜色更改【输入输出流】 
+ostream&operator<<(ostream&outStream,const Color&_COLOR){
+	_SetColor(_COLOR);
+	return outStream;
+}
+istream&operator>>(istream&inStream,const Color&_COLOR){
+	_SetColor(_COLOR);
+	return inStream;
+}
